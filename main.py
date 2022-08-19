c5d9651101fbdbcf1f97b3884309ce6b6c9013f9
sameer <sameersahu200138@gmail.com>
2022-08-19 23:58:17 +0530

    made some changes to main.py

--- main.py
+++ main.py
@@ -2,29 +2,17 @@ import praw
 import os
 import random
 import time
+import json
 
-titles = [
-    "ranting about jira",
-    "siting in a meeting",
-    "squashing bugs",
-    "centering divs",
-    "waiting for staging deployment",
-    "writing tests",
-    "pushing directly to prod",
-    "doing code review",
-    "deleting jira tickets",
-    "Creating a pull request",
-    "forgot to run DB migrations",
-    "Broke production",
-    "running on staging ENV",
-    "merging git branches",
-    "releasing the MVP",
-    "finding JIRA tickets"
-]
+with open('dataset.json', 'r') as f:
+    data = json.load(f)
+
+titles = data['titles']
 
 client_id = os.environ["REDDIT_CLIENT_ID"]
 client_secret = os.environ["REDDIT_CLIENT_SECRET"]
 reddit_pass = os.environ["REDDIT_PASSWORD"]
+
 
 def get_titles():
     currentlyViewingText, subscribersText = random.sample(titles, 2)
@@ -52,5 +40,6 @@ def update_titles():
     widgets.id_card.mod.update(currentlyViewingText=titles[0])
     widgets.refresh()
     widgets.id_card.mod.update(subscribersText=titles[1])
+
 
 update_titles()

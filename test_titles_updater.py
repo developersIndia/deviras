981bd089b51c23618bdb788834b78be663e49738
Animesh-Ghosh <ani101098@yahoo.in>
2022-08-21 02:04:05 +0530

    Add Tests for get_titles function
    Added docstring for main.py script and updated it to only invoke update_titles
    function if the file was run directly.
    Added initial tests for titles updater script.

---
+++ test_titles_updater.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest.mock import patch
+
+# patch out the environ dictionary
+# used to instantiate global variables in the titles_updater script
+# maybe move the call to os.environ in the script to the update_titles method?
+environ_patcher = patch.dict('os.environ', {
+    'REDDIT_CLIENT_ID': '',
+    'REDDIT_CLIENT_SECRET': '',
+    'REDDIT_PASSWORD': ''
+})
+environ_patcher.start()
+
+from main import get_titles
+
+# TODO: write tests for update_titles method after figuring out
+# which objects to mock and with what
+class TestTitlesUpdater(unittest.TestCase):
+    @patch('main.json.load', return_value={'titles': ['foo', 'bar', 'baz']})
+    def test_get_titles_returns_a_list(self, _):
+        titles = get_titles()
+        self.assertIsInstance(titles, list)
+
+    @patch('main.json.load', return_value={'titles': ['foo', 'bar', 'baz']})
+    def test_get_titles_contains_titles_from_dataset_file(self, mock_json_load):
+        titles = get_titles()
+
+        self.assertTrue(all(map(lambda title: title in mock_json_load.return_value['titles'], titles)))
+
+environ_patcher.stop()

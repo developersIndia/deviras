dda14476df662e3e80220be759c32a6b8224f724
Bhupesh-V <varshneybhupesh@gmail.com>
2023-12-28 22:47:40 +0530

    add flair to showcase sunday thread

--- showcase-sunday/main.py
+++ showcase-sunday/main.py
@@ -7,6 +7,7 @@ client_secret = os.environ["REDDIT_CLIENT_SECRET"]
 reddit_pass = os.environ["REDDIT_PASSWORD"]
 username = os.environ["REDDIT_USERNAME"]
 user_agent = 'Showcase Sunday Megathread'
+sub = "developersIndia"
 
 def is_second_sunday():
     today = datetime.date.today()
@@ -17,17 +18,29 @@ def is_second_sunday():
     return today == second_sunday
 
 
-def create_showcase_sunday_megathread(reddit):
-    subreddit = reddit.subreddit("developersIndia")
+def create_showcase_sunday_megathread(subreddit):
+    flair = next(
+        filter(
+            lambda flair: flair["flair_text"] == "Showcase Sunday",
+            subreddit.flair.link_templates.user_selectable(),
+        )
+    )
+       
     title = "Showcase Sunday Megathread - {month} {year}".format(month=datetime.date.today().strftime("%B"), year=datetime.date.today().year)
     text = """
-Welcome to the Showcase Sunday Megathread!
+It's time for our monthly showcase thread where we celebrate the incredible talent in our community. Whether it's an app, a website, a tool, or anything else you've built, we want to see it! Share your latest creations, side projects, or even your work-in-progress.
+
+Let's inspire each other and celebrate the diverse skills we have. Comment below with details about what you've built, the tech stack used, and any interesting challenges faced along the way.
 """
-    sticky = subreddit.submit(title, selftext=text, send_replies=False)
-    sticky.mod.sticky(state=True, bottom=True)
-    # sticky.mod.suggested_sort(sort='new')
-    sticky.mod.flair(text="Megathread")
-    return sticky.id
+
+    submission = subreddit.submit(
+        title,
+        selftext=text,
+        flair_id=flair["flair_template_id"],
+    )
+    submission.mod.sticky()
+
+    return submission
 
 def main():
     reddit = praw.Reddit(
@@ -38,8 +51,10 @@ def main():
         user_agent=user_agent
     )
 
+    subreddit = reddit.subreddit(sub)
+
     if is_second_sunday():
-        create_showcase_sunday_megathread(reddit)
+        create_showcase_sunday_megathread(subreddit)
 
 if __name__ == "__main__":
     main()

ca4e3f16f63bd6bae24233e9efa8fbc53108c890
Nisarga Adhikary <git@nisarga.me>
2024-01-20 16:28:20 +0530

    Update main.py

--- ama-summarizer/main.py
+++ ama-summarizer/main.py
@@ -1,42 +1,60 @@
 import praw
+import os
 from dotenv import dotenv_values
 
+def get_reddit_instance():
+    # Reddit API credentials
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 14.2; rv:109.0) Gecko/20100101 Firefox/121.0'
+    client_id = os.environ["REDDIT_CLIENT_ID"]
+    client_secret = os.environ["REDDIT_CLIENT_SECRET"]
+    reddit_pass = os.environ["REDDIT_PASSWORD"]
+    username = os.environ["REDDIT_USERNAME"]
 
-# Reddit API credentials
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 14.2; rv:109.0) Gecko/20100101 Firefox/121.0'
-client_id = os.environ["REDDIT_CLIENT_ID"]
-client_secret = os.environ["REDDIT_CLIENT_SECRET"]
-reddit_pass = os.environ["REDDIT_PASSWORD"]
-username = os.environ["REDDIT_USERNAME"]
+    # Create a Reddit instance
+    reddit = praw.Reddit(client_id=client_id,
+                         client_secret=client_secret,
+                         password=reddit_pass,
+                         user_agent=user_agent,
+                         username=username)
+    return reddit
 
-# Create a Reddit instance
-reddit = praw.Reddit(client_id=client_id,
-                     client_secret=client_secret,
-                     password=reddit_pass,
-                     user_agent=user_agent,
-                     username=username)
 
-post_url = 'https://reddit.com/r/developersIndia/' # replace with AMA post URL
+def get_post_url():
 
-submission = reddit.submission(url=post_url)
-submission.comments.replace_more(limit=None)
+    post_url = input("Enter the AMA post URL: ") # reddit.com URLs preferred 
+    return post_url
 
-guest_username = 'AMA_GUEST_USERNAME' # replace with AMA guest name
-markdown_file = ''
-question_number = 1
+def get_guest_username():
 
-for comment in submission.comments.list():
-    if comment.author and comment.author.name.lower() == guest_username.lower():
-        question_text = comment.parent().body.replace('\n', ' ')
-        question_link = 'https://reddit.com' + comment.parent().permalink
-        markdown_file += f'{question_number}. [{question_text}]({question_link})\n'
-        question_number += 1
+    guest_username = input("Enter the AMA guest username: ") 
+    return guest_username
 
-# UWU
+def main():
+    reddit = get_reddit_instance()
+    
+    post_url = get_post_url()
+    guest_username = get_guest_username()
 
-# UTF-8 encoding
-with open('questions.md', 'w', encoding='utf-8') as file:
-    file.write(markdown_file)
+    submission = reddit.submission(url=post_url)
+    submission.comments.replace_more(limit=None)
 
-print('Markdown file generated successfully.')
+    markdown_file = ''
+    question_number = 1
+
+    for comment in submission.comments.list():
+        if comment.author and comment.author.name.lower() == guest_username.lower():
+            question_text = comment.parent().body.replace('\n', ' ')
+            question_link = 'https://reddit.com' + comment.parent().permalink
+            markdown_file += f'{question_number}. [{question_text}]({question_link})\n'
+            question_number += 1
+
+    # UTF-8 encoding
+    with open('questions.md', 'w', encoding='utf-8') as file:
+        file.write(markdown_file)
+
+    print('Markdown file questions.md generated successfully.')
+
+if __name__ == "__main__":
+    main()
+
 
